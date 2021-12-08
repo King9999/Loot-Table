4703ea72b32a09a0bff1fa6e13324e68f5079663
Mike Murray <mikemurray056@gmail.com>
2021-12-08 11:15:30 -0500

    Added more enemies. Changed enemy level to rank
    I couldn't think of a good reason for enemies to have a level so instead enemies are grouped by a rank from D to S. D is lowest level, S is highest.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 //enemies consist only of a level and a sprite.    
 public class Enemy : MonoBehaviour
 {
-    public int enemyLevel;          //determines which tables are accessible.
+    public char enemyRank;          //determines which tables are accessible.
     public int tableId;             //the enemy's drop table. It's determined by enemy level.
-    public Sprite enemySprite;
+    //public Sprite enemySprite;
     public bool isRare;             //if true, then two rolls occur for an item. Better result is picked.
 
     //consts
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 using TMPro;
 
+/* Enemies are defeated, and items have a chance to drop. Sometimes, a rare version of an enemy appears, which
+ * allows two chances for an item to drop. The better roll is picked, and if nothing drops, the added weight bonus is
+ * tripled. */
 public class GameManager : MonoBehaviour
 {
     public TableReader reader;
-    public Enemy enemy;
-    public Sprite playerSprite;
+    //public Enemy enemy;
+    //public Sprite playerSprite;
 
     public Enemy[] enemies1;    //level 1 to 10
+    public Enemy[] enemies2;    //level 11 to 20
+    public Enemy[] enemies3;    //level 21 to 30
+    public Enemy[] enemies4;    //level 31 to 40
+    public Enemy[] enemies5;    //level 41 to 50
 
     public float currentTime;
     public float getItemTime = 1;       //time in seconds to generate item.
